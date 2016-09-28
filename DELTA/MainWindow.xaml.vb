69116260cc9b1cef515762a29b5160e7a1f805fd
Felipe Ramos <pipoblakgamer@gmail.com>
2016-09-28 09:16:54 -0300

    First Commit

---
+++ DELTA/MainWindow.xaml.vb
@@ -0,0 +1,55 @@
+﻿Class MainWindow
+    Dim verifMove As Boolean = True
+    Dim animationLoading As Animation.Storyboard
+
+    Private Sub lblClose_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles lblClose.PreviewMouseLeftButtonDown
+
+        Me.Close()
+    End Sub
+
+    Private Sub window_MouseUp(sender As Object, e As MouseButtonEventArgs) Handles window.MouseUp
+        Me.Cursor = Cursors.Arrow
+    End Sub
+
+    Private Sub brdLogin_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles brdLogin.MouseLeftButtonDown
+        If verifMove Then
+            Me.Cursor = Cursors.SizeAll
+            Me.DragMove()
+            Keyboard.ClearFocus()
+        End If
+
+    End Sub
+
+    Private Sub lblRegistrar_MouseEnter(sender As Object, e As MouseEventArgs) Handles lblRegistrar.MouseEnter
+        verifMove = False
+    End Sub
+    Private Sub lblRegistrar_MouseLeave(sender As Object, e As MouseEventArgs) Handles lblRegistrar.MouseLeave
+        verifMove = True
+
+    End Sub
+
+    Private Sub lblConectar_MouseEnter(sender As Object, e As MouseEventArgs) Handles lblConectar.MouseEnter
+        verifMove = False
+    End Sub
+    Private Sub lblConectar_MouseLeave(sender As Object, e As MouseEventArgs) Handles lblConectar.MouseLeave
+        verifMove = False
+    End Sub
+
+    Private Sub lblConectar_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles lblConectar.MouseLeftButtonDown
+        Try
+
+            MsgBox(animationLoading.GetIsPaused)
+        Catch ex As Exception
+            animationLoading.Begin()
+        End Try
+
+
+
+
+    End Sub
+
+    Private Sub window_Loaded(sender As Object, e As RoutedEventArgs) Handles window.Loaded
+        animationLoading = TryFindResource("LoadingLogo")
+
+    End Sub
+End Class
